886f2918a4b040ab1e75becb0bd302316861071d
John Darragh <darragh@entrotech.net>
2016-12-05 20:32:44 -0800

    Add project files.

---
+++ Talent.WpfClient/PeopleViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Talent.DataAccess.Ado;
+using Talent.Domain;
+using UclaExt.Common.BaseClasses;
+
+namespace Talent.WpfClient
+{
+    public class PeopleViewModel : DomainViewModel<Person>
+    {
+
+        public PeopleViewModel() : base( new PersonRepository())
+        {
+        }
+
+        #region Lookup Lists
+
+        public ObservableCollection<HairColor> HairColors
+        {
+            get {
+                return new ObservableCollection<HairColor>( 
+                    LookupCache.HairColors);
+            }
+        }
+
+        public ObservableCollection<EyeColor> EyeColors
+        {
+            get
+            {
+                return new ObservableCollection<EyeColor>(
+                    LookupCache.EyeColors);
+            }
+        }
+
+        public ObservableCollection<CreditType> CreditTypes
+        {
+            get
+            {
+                return new ObservableCollection<CreditType>(
+                    LookupCache.CreditTypes);
+            }
+        }
+
+        public ObservableCollection<Show> Shows
+        {
+            get
+            {
+                return new ObservableCollection<Show>(
+                    LookupCache.Shows);
+            }
+        }
+
+        #endregion
+
+    }
+}
